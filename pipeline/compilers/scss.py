d88b0eafee4d6e4bec280a955293ed23ee61f570
Kenneth Lee <kenneth@twg.ca>
2012-06-01 11:46:46 -0400

    fixed sass compiler

--- pipeline/compilers/scss.py
+++ pipeline/compilers/scss.py
@@ -4,14 +4,14 @@ from pipeline.conf import settings
 from pipeline.compilers import SubProcessCompiler
 
 
-class SASSCompiler(SubProcessCompiler):
+class SCSSCompiler(SubProcessCompiler):
     output_extension = 'css'
 
     def match_file(self, filename):
-        return filename.endswith(('.sass'))
+        return filename.endswith(('.scss'))
 
     def compile_file(self, content, path):
-        command = "%s %s %s" % (
+        command = "%s --scss %s %s" % (
             settings.PIPELINE_SASS_BINARY,
             settings.PIPELINE_SASS_ARGUMENTS,
             path
